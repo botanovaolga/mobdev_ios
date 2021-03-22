31e8e6546b4729baa6fd838bbb4d1d81b2f4ff3c
Botanova Olga <botanova.olga@gmail.com>
2021-03-22 19:17:24 +0300

    Task 3 done

--- Notes/Notes/NoteViewController.swift
+++ Notes/Notes/NoteViewController.swift
@@ -8,22 +8,19 @@
 import UIKit
 
 class NoteViewController: UIViewController {
+    
+    @IBOutlet var titleLable: UILabel!
+    @IBOutlet var noteLable: UITextView!
+    
+    public var noteTitle: String = ""
+    public var note: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        titleLable.text = noteTitle
+        noteLable.text = note
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
